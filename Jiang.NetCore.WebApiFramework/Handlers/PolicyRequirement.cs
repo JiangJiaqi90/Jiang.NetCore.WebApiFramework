fa4966b58e03debedb8f026ef902e894fbd0b8fe
jiangjiaqi <610877596@qq.com>
2019-02-15 17:58:46 +0800

    edit annotation

--- Jiang.NetCore.WebApiFramework/Handlers/PolicyRequirement.cs
+++ Jiang.NetCore.WebApiFramework/Handlers/PolicyRequirement.cs
@@ -2,8 +2,6 @@
 // <copyright file= "PolicyRequirement.cs">
 //     Copyright (c) Danvic712. All rights reserved.
 // </copyright>
-// Author: Danvic712
-// Created DateTime: 2019/1/15 19:22:05 
 // Modified by:
 // Description: 自定义策略要求
 //-----------------------------------------------------------------------
